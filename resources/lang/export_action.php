99d301cb748f4c0ffc7330f78b77725c2059a28d
inerba <inerba@gmail.com>
2022-12-27 15:40:24 +0100

    Italian translation

---
+++ resources/lang/export_action.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'action_label' => 'Esporta',
+    'modal_heading' => 'Esporta',
+    'file_name_field_label' => 'Nome del file',
+    'format_field_label' => 'Formato',
+    'page_orientation_field_label' => 'Orientamento della pagina',
+    'filter_columns_field_label' => 'Filtra colonne',
+    'additional_columns_field' => [
+        'label' => 'Colonne aggiuntive',
+        'title_field_label' => 'Titolo',
+        'default_value_field_label' => 'Valore predefinito',
+        'add_button_label' => 'Aggiungi colonna',
+    ],
+    'export_action_label' => 'Esporta',
+    'print_action_label' => 'Stampa',
+    'preview_action_label' => 'Anteprima',
+    'cancel_action_label' => 'Cancella',
+    'page_orientation_portrait' => 'Ritratto',
+    'page_orientation_landscape' => 'Panoramica',
+];
