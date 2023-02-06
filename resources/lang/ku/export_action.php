09ec3a6cc645c82b02051dfde514ba178489d3f4
AlanD20 <aland20@pm.me>
2023-02-06 01:19:34 +0100

    Add Kurdish translation

---
+++ resources/lang/ku/export_action.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'action_label' => 'ڕاپۆڕت',
+    'modal_heading' => 'ڕاپۆڕت',
+    'file_name_field_label' => 'ناوی فایل',
+    'format_field_label' => 'جۆر',
+    'page_orientation_field_label' => 'ئاڕاستەی پەڕە',
+    'filter_columns_field_label' => 'فلتەری خانەکان',
+    'additional_columns_field' => [
+        'label' => 'خانەی زیادە',
+        'title_field_label' => 'ناوی خانە',
+        'default_value_field_label' => 'نرخی خانە',
+        'add_button_label' => 'زیادکردنی خانە',
+    ],
+    'export_action_label' => 'دابەزاندن',
+    'print_action_label' => 'چاپکردن',
+    'preview_action_label' => 'بینین',
+    'cancel_action_label' => 'ڕەتکردنەوە',
+    'page_orientation_portrait' => 'پەڕگەی ستوونی',
+    'page_orientation_landscape' => 'پەڕگەی ئاسۆیی',
+];
---
+++ resources/lang/ku/table_view.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'preview_modal_heading' => 'بینین',
+    'print_action_label' => 'چاپکردن',
+    'export_action_label' => 'دابەزاندن',
+];
