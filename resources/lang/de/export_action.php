98e1523fbf954a989e3901099e308ffd9a7183b3
Josef Behr <code@josef-behr.de>
2023-02-08 19:27:15 +0100

    add german translations

---
+++ resources/lang/de/export_action.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'action_label' => 'Exportieren',
+    'modal_heading' => 'Exportieren',
+    'file_name_field_label' => 'Dateiname',
+    'format_field_label' => 'Format',
+    'page_orientation_field_label' => 'Seitenorientierung',
+    'filter_columns_field_label' => 'Spalten für Filterung',
+    'additional_columns_field' => [
+        'label' => 'Zusätzliche Spalten',
+        'title_field_label' => 'Titel',
+        'default_value_field_label' => 'Standardwert',
+        'add_button_label' => 'Spalte hinzufügen',
+    ],
+    'export_action_label' => 'Exportieren',
+    'print_action_label' => 'Drucken',
+    'preview_action_label' => 'Vorschau',
+    'cancel_action_label' => 'Abbrechen',
+    'page_orientation_portrait' => 'Hochformat',
+    'page_orientation_landscape' => 'Querformat',
+];
