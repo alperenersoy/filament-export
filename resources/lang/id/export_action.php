02890288e15937499a411e85213cb81b43cc6b11
Ruswan <sukabumikode@gmail.com>
2023-05-21 05:45:11 +0700

    The addition of Indonesian language

---
+++ resources/lang/id/export_action.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'action_label' => 'Ekspor',
+    'modal_heading' => 'Ekspor',
+    'file_name_field_label' => 'Nama File',
+    'format_field_label' => 'Format',
+    'page_orientation_field_label' => 'Orientasi Halaman',
+    'filter_columns_field_label' => 'Filter Kolom',
+    'additional_columns_field' => [
+        'label' => 'Kolom Tambahan',
+        'title_field_label' => 'Judul',
+        'default_value_field_label' => 'Nilai Bawaan',
+        'add_button_label' => 'Tambah Kolom',
+    ],
+    'export_action_label' => 'Ekspor',
+    'print_action_label' => 'Cetak',
+    'preview_action_label' => 'Pratinjau',
+    'cancel_action_label' => 'Batal',
+    'page_orientation_portrait' => 'Potret',
+    'page_orientation_landscape' => 'Lanskap',
+];
