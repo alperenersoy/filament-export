0fd8cd494db752e75dcb4bf9f6b083924e42b292
alwayshopeless <38844539+alwayshopeless@users.noreply.github.com>
2023-10-03 05:22:56 +0300

    Update TableView.php
    Submit method requires form name string.

--- src/Components/TableView.php
+++ src/Components/TableView.php
@@ -140,7 +140,7 @@ class TableView extends Component
         return Action::make('export')
             ->button()
             ->label(__('filament-export::table_view.export_action_label'))
-            ->submit()
+            ->submit("export")
             ->icon(config('filament-export.export_icon'));
     }
 
